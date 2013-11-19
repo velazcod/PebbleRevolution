aa118b8ee2e5490e3bda9c852fae84066c704723
Daniel Velazco <velazcod@gmail.com>
2013-11-18 22:47:18 -0800

    Always use american date format

--- src/Revolution.c
+++ src/Revolution.c
@@ -11,7 +11,7 @@
 
 // Settings
 #define USE_QUICKTAP_PLUS             true
-#define USE_AMERICAN_DATE_FORMAT      false
+#define USE_AMERICAN_DATE_FORMAT      true
 #define VIBE_ON_HOUR                  false
 #define TIME_SLOT_ANIMATION_DURATION  500
 
